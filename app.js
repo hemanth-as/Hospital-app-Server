aabddc465b5ff851c8bbb98918ad30f3570dc226
Hemanth A S <115790250+hemanth-as@users.noreply.github.com>
2022-12-24 22:44:56 +0530

    extended submission
    completed

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
-const express=require('express');
+const express=require("express");
 const app=new express();
 const data=require('./datas.json');
-app.get("/hospital", (req, res) => {
+app.use(express.json());
+const fs=require("fs");
+
+app.get('/hospital',(req,res)=>{
     res.send(data);
 })
 
+app.post('/hospital',(req,res)=>{
+    data.push(req.body);
+    fs.writeFile("datas.json", JSON.stringify(data),(err, resp)=>{
+        if(err){
+            res.send("Data can not be written");
+        }
+        else{
+            res.send("Data wriiten successfully");
+        }
+    })
+});
+
+app.put('/hospital/:name',(req,res)=>{
+    let name=req.params.name;
+    data.forEach((item)=>{
+        if(item.name==name){
+            item.count=req.body.count;
+            item.location=req.body.location;
+        }
+    })
+    fs.writeFile("datas.json",JSON.stringify(data), (err,resp)=>{
+        if(err){
+            res.send("Data can not be updated");
+        }
+        else{
+            res.send("Data updated succesfully");
+        }
+    })
+});
+
+app.delete('/hospital/:name',(req,res)=>{
+    let name=req.params.name;
+    let value=data.filter(item=>item.name!==name);
+    fs.writeFile("datas.json",JSON.stringify(value),(err,resp)=>{
+        if(err){
+            res.send("Data can not be deleted")
+        }
+        else{
+            res.send("Data deleted succesfully");
+        }
+    })
+});
 
 app.listen(3000);
 console.log("Server listening to port 3000");
